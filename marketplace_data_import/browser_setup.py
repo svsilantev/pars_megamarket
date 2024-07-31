9a78e5d8a8387ac035c88cb5c106641968d997f5
svsilantev <ssilantev@gmail.com>
2024-07-31 13:04:21 +0300

    СЂРµР¶РёРј Р±РµР· Р·Р°РіРѕР»РѕРІРєРѕРІ

--- marketplace_data_import/browser_setup.py
+++ marketplace_data_import/browser_setup.py
@@ -28,7 +28,7 @@ else:
 
 service = ChromeService(executable_path=chrome_driver_path)
 options = webdriver.ChromeOptions()
-# options.add_argument("--headless")
+options.add_argument("--headless")
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 options.add_argument('--disable-gpu')
