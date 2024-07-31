51709fa164def9c622d522220798bc9ee25cff08
svsilantev <ssilantev@gmail.com>
2024-07-31 18:23:24 +0300

    Р±РѕСЂСЊР±Р° СЃ Р±РµР·РіРѕР»РѕРІС‹Рј СЂРµР¶РёРјРѕРј

--- marketplace_data_import/browser_setup.py
+++ marketplace_data_import/browser_setup.py
@@ -25,7 +25,7 @@ def init_driver():
     if platform.system() == "Linux":
         options.binary_location = "/usr/bin/google-chrome"
         options.add_argument('--remote-debugging-port=9222')
-    # options.add_argument("--headless")
+    # options.add_argument("--headless")  # Убедитесь, что этот аргумент закомментирован
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--disable-gpu')
